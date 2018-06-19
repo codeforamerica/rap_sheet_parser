ef240cc66b868efe30ea1103a9d6e558e0f5d162
Symonne Singleton <symonne@codeforamerica.org>
2018-06-19 16:22:04 -0700

    Add South SF to known courthouses
    [#158367375]
    
    Signed-off-by: Paras Sanghavi <paras@codeforamerica.org>
    Co-authored-by: Paras Sanghavi <paras@codeforamerica.org>

--- lib/rap_sheet_parser/builders/courthouse_presenter.rb
+++ lib/rap_sheet_parser/builders/courthouse_presenter.rb
@@ -5,6 +5,7 @@ module RapSheetParser
         'CASC SAN FRANCISCO CO' => 'CASC San Francisco Co',
         'CASC SAN FRANCISCO' => 'CASC San Francisco',
         'CAMC SAN FRANCISCO' => 'CAMC San Francisco',
+        'CAMC SOUTH SAN FRANCISCO' => 'CAMC South San Francisco',
         'CAMC RICHMOND' => 'CAMC Richmond',
         'CASC MCRICHMOND' => 'CASC Richmond',
         'CAMC CONCORD' => 'CAMC Concord',
